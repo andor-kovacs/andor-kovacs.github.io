588a05764109f075cf99c5a2e8c773cdeb032211
andor-kovacs <kovacsandor86@gmail.com>
2022-11-01 16:22:15 +0100

    skills width

--- ka.js
+++ ka.js
@@ -1,22 +1,22 @@
 //---------------------click on the button and jump onto the top-----------
-//Get the button
-var mybutton = document.getElementById("up");
+// //Get the button
+// var mybutton = document.getElementById("up");
 
-// When the user scrolls down 100px from the top of the document, show the button
-window.onscroll = function () { scrollFunction() };
+// // When the user scrolls down 100px from the top of the document, show the button
+// window.onscroll = function () { scrollFunction() };
 
-function scrollFunction() {
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
-}
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
+// function scrollFunction() {
+//     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+//         mybutton.style.display = "block";
+//     } else {
+//         mybutton.style.display = "none";
+//     }
+// }
+// // When the user clicks on the button, scroll to the top of the document
+// function topFunction() {
+//     document.body.scrollTop = 0;
+//     document.documentElement.scrollTop = 0;
+// }
 
 // ----------------------------hamburgermenu-close-and-open--------------------
 
