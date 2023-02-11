54a9f2341a96d102e8926730f23265d7891dffbd
andor-kovacs <kovacsandor86@gmail.com>
2023-02-11 15:09:51 +0100

    language update

--- ka.js
+++ ka.js
@@ -1,29 +1,7 @@
-//---------------------click on the button and jump onto the top-----------
-// //Get the button
-// var mybutton = document.getElementById("up");
-
-// // When the user scrolls down 100px from the top of the document, show the button
-// window.onscroll = function () { scrollFunction() };
-
-// function scrollFunction() {
-//     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-//         mybutton.style.display = "block";
-//     } else {
-//         mybutton.style.display = "none";
-//     }
-// }
-// // When the user clicks on the button, scroll to the top of the document
-// function topFunction() {
-//     document.body.scrollTop = 0;
-//     document.documentElement.scrollTop = 0;
-// }
-
-// ----------------------------hamburgermenu-close-and-open--------------------
-
 function hideMenu() {
     let menuOpen = document.querySelector('.checkbox').checked;
 
-    if (menuOpen = true) {
+    if (menuOpen === true) {
         document.querySelector('.checkbox').checked = false;
     }
 }
